54620058978abcc3c44d5a968ba68e51a94301bb
faris808 <fariszama2019@gmail.com>
2024-12-09 15:44:14 +0530

    updated code

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -48,7 +48,7 @@ export const sendVerificationEmail = async (user, res) => {
     </p>
     <div style="margin-top: 20px;">
         <h5>Best Regards</h5>
-        <h5>ShareFun Team</h5>
+        <h5>SocialWave Team</h5>
     </div>
 </div>`,
   };
